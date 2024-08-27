bddaa2e4072870cb1409823df6eb33e3352f52f3
tokyodreamz <steambeardz@gmail.com>
2024-08-27 11:22:00 +0300

    Add files via upload

---
+++ Task_3.py
@@ -0,0 +1,5 @@
+Number_of_executed_tasks = 12
+Number_of_Hours_Earned = 1.5
+Course_name = 'Python'
+Time_for_one_assignment = Number_of_Hours_Earned / Number_of_executed_tasks
+print(f"Курс: {Course_name}, всего задач: {Number_of_executed_tasks}, затрачено часов: {Number_of_Hours_Earned}, среднее время выполнения {Time_for_one_assignment:.3f} часа.")
