541606d39bca97a129558f36ce75082744a4ddf3
ALFA <steambeardz@gmail.com>
2024-08-28 23:37:29 +0300

    Task_4

---
+++ TEST.py
@@ -0,0 +1,7 @@
+#name = input("Введите ваше имя: ")
+#current_year = 2024
+#date_of_birth = input("В каком году вы родились? ")
+#age = current_year - int(date_of_birth)
+#print("Здравствуйте, ", name)
+#print("В этом году вам", age, "лет")
+print('Hello, i am biba'.replace())
