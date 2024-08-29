5a4b7d252392b76057eae13f5ab936ebc4de65c3
ALFA <steambeardz@gmail.com>
2024-08-29 14:31:16 +0300

    Корректировка

--- Module_1_5.py
+++ Module_1_5.py
@@ -1,8 +1,5 @@
-from xml.dom.minidom import ProcessingInstruction
-
 immutable_var = ("Boom", 1, 3.14, True)
 print(immutable_var)
-
 try:
  immutable_var[1] = "World"
 except TypeError as e:print("Ошибка при попытке изменить элемент кортежа:", e)
