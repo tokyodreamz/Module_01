cc5357c44f957fad160bd7bfcd79e88087fe948e
ALFA <steambeardz@gmail.com>
2024-09-01 14:29:09 +0300

    Reliase_01

--- TEST.py
+++ TEST.py
@@ -4,4 +4,59 @@
 #age = current_year - int(date_of_birth)
 #print("Здравствуйте, ", name)
 #print("В этом году вам", age, "лет")
-print('Hello, i am biba'.replace())
+#print('Hello, i am biba'.replace())
+#food = ["apple", "coconut", "banana"]
+#food.append(True)
+#print(food)
+#food.extend(["string", 2])
+#print(food)
+#food.remove("apple")
+#print(food)
+#print("coconut" not in food)
+#print(food[0:2:2])
+#tuple_ = 1,2,3, True, "String"
+#list_ = 1,2,3, True, "String"
+#tuple_2 = (1,2,3,4)
+#tuple_3 = tuple([1,2,3,4])
+#print(tuple_.__sizeof__())
+#print(list_.__sizeof__())
+#tuple_ = ([1, 2], 0) + (1, 2)
+#print(tuple_)
+#tuple_[0][0] = 2
+#print(tuple_)
+#tuple_ = (1,2) * 3
+#print(tuple_)
+#a = (1,2,[3,4])
+#a[2].append(5)
+#print(a)
+from multiprocessing.connection import deliver_challenge
+
+#from Vebinar_01 import set_size
+
+#phone_book = {'Kirill' : [89269163424, 7349793], 'Pasha' : 89261778211}
+#phone_book['Kirill'] = 128180481
+#phone_book['Denis'] = 848184185
+#del  phone_book['Pasha']
+#phone_book.update({'Sasha' : 9041895482095,
+                   #'Alex' : 9089563860468,})
+                   #'Kirill' : 908408084})
+#print(phone_book.get('Afonya', 'Такого ключа нет'))
+#print(phone_book)
+#a = phone_book.pop('Denis')
+#list_ = [1, 2, 3]
+#list_.pop(0)
+#print(list_)
+#print(phone_book)
+#print(a)
+#print(phone_book.keys())
+#print(phone_book.values())
+#print(phone_book)
+set_ = {1,2,3,4,5,1,2,3,4,'String', True, (1,2,3)}
+list_ = [1,1,1,1,11]
+list_ = set(list_)
+print(list_)
+#print(list_.discard(1))
+print(list_.remove(1))
+print(list_.add(5))
+print(list_)
+
