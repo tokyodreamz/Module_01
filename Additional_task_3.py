81cf7e938d31002565254b589b886be271bab951
ALFA <steambeardz@gmail.com>
2024-09-20 11:42:50 +0300

    1

---
+++ Additional_task_3.py
@@ -0,0 +1,30 @@
+def calculate_structure_sum(data):
+    total_sum = 0
+
+    def recursive_sum(item):
+        nonlocal total_sum
+        if isinstance(item, int):
+            total_sum += item
+        elif isinstance(item, str):
+            total_sum += len(item)
+        elif isinstance(item, list) or isinstance(item, tuple) or isinstance(item, set):
+            for sub_item in item:
+                recursive_sum(sub_item)
+        elif isinstance(item, dict):
+            for key, value in item.items():
+                recursive_sum(key)
+                recursive_sum(value)
+
+    recursive_sum(data)
+    return total_sum
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
