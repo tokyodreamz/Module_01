89ee00bc32b3a24147bbad12851dbb6628222406
ALFA <steambeardz@gmail.com>
2024-10-21 16:38:02 +0300

    1

---
+++ Module_7_1.py
@@ -0,0 +1,53 @@
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        try:
+            with open(self.__file_name, 'r', encoding='utf-8') as file:
+                products = file.read().strip()
+            return products
+        except FileNotFoundError:
+            return ''
+
+    def add(self, *products):
+        existing_products = self.get_products().split('\n')
+        existing_product_names = {product.split(', ')[0] for product in existing_products if product}
+
+        with open(self.__file_name, 'a', encoding='utf-8') as file:
+            for product in products:
+                if product.name not in existing_product_names:
+                    file.write(str(product) + '\n')
+                else:
+                    print(f'Продукт {product} уже есть в магазине')
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
